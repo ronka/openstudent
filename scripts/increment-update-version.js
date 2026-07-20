534b9e0fe1d53edbdacb2bfa4a57d04d192fe2e5
ronka <ronkamail@gmail.com>
2026-07-20 22:35:08 +0300

    update script to auto commit

--- scripts/increment-update-version.js
+++ scripts/increment-update-version.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs');
 const path = require('path');
+const { execSync } = require('child_process');
 
 function incrementUpdateVersion() {
   // Accept a custom settings path as CLI arg, or search common locations
@@ -51,6 +52,9 @@ function incrementUpdateVersion() {
   fs.writeFileSync(settingsPath, updatedContents);
 
   console.log(`UPDATE_VERSION bumped from ${currentVersion} to ${nextVersion} in ${settingsPath}`);
+
+  execSync(`git add ${JSON.stringify(settingsPath)}`, { stdio: 'inherit' });
+  execSync(`git commit -m ${JSON.stringify(`Bump UPDATE_VERSION to ${nextVersion}`)}`, { stdio: 'inherit' });
 }
 
 incrementUpdateVersion();
